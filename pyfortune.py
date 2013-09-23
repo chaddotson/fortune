d3fbf6fc5e34c23940c12ac5c3bb2e0a5ff9615e
Chad Dotson <chad.dotson@gmail.com>
2013-09-23 00:13:39 -0500

    Tweaking...

--- pyfortune.py
+++ pyfortune.py
@@ -12,13 +12,9 @@ fortune_files = sys.argv[1:]
 possible_fortunes = []
 
 for fortune_file in fortune_files:
-    
     with open(fortune_file, 'r') as file:
         possible_fortunes.extend([quote.replace(r'\n','').strip() for quote in file.readlines() if "%" not in quote])
     
 random.shuffle(possible_fortunes);
 
-
 print possible_fortunes[random.randint(0,len(possible_fortunes)-1)]
-
-
